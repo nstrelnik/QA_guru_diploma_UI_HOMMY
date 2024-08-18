46433cfde6bf629c0aa19085d24c6a4659b93bb2
Anastasiia Strelnik <n.strelnik@mail.ru>
2024-08-18 19:15:12 +0200

    delete comments

--- data/users.py
+++ data/users.py
@@ -1,18 +1,7 @@
 import dataclasses
 
-# @dataclass
-# class User:
-#     login: str
-#     password: str
-#     name: Optional[str] = None
-#     country: Optional[str] = None
-#     city: Optional[str] = None
-#     credit_card: Optional[str] = None
-#     month: Optional[str] = None
-#     year: Optional[str] = None
 
 @dataclasses.dataclass
 class User:
     user_email: str
     user_password: str
-
